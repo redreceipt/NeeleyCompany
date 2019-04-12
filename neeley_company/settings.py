e860e73a3042d578d8f42970c148b86936879e04
Michael Neeley <micneeley14@gmail.com>
2019-04-11 20:07:55 -0400

    got rid of sqlite db

--- neeley_company/settings.py
+++ neeley_company/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
     "default": {
         # "ENGINE": "django.db.backends.sqlite3",
         "ENGINE": "",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
